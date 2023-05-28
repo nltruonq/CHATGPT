483219f266900a12f9c22b369fe9f89673d06adb
20120612 <nguyenlamtruong20021@gmail.com>
2023-05-29 00:32:57 +0700

    fix

--- configs/openai.js
+++ configs/openai.js
@@ -1,12 +1,13 @@
-const { Configuration, OpenAIApi } = require('openai');
+const { Configuration, OpenAIApi } = require("openai");
 
 const configOpenai = () => {
     const configuration = new Configuration({
         organization: process.env.OPENAI_API_ORGANIZATION_ID,
-        apiKey: process.env.OPENAI_API_KEY
+        apiKey: process.env.OPENAI_API_KEY,
     });
-    
-    return new OpenAIApi(configuration);
-}
 
-module.exports = configOpenai;
+    return new OpenAIApi(configuration);
+};
+const openai = configOpenai();
+
+module.exports = openai;
--- controllers/chatgptController.js
+++ controllers/chatgptController.js
@@ -1,20 +1,18 @@
-const configOpenai = require("../configs/openai");
+const openai = require("../configs/openai");
 
 const chatgptController = {
     chat: async (req, res) => {
         try {
             const content = req.body.content;
-            const openai = configOpenai();
             const response = await openai.createChatCompletion({
                 model: "gpt-3.5-turbo",
                 messages: [{ role: "user", content: content }],
                 temperature: 0.5,
-                frequency_penalty: 0.5,
-                presence_penalty: 0.4,
-                max_tokens: 100,
+                frequency_penalty: 0,
+                presence_penalty: 0,
+                max_tokens: 500,
                 top_p: 1,
             });
-            console.log(response);
             return res.status(200).json({
                 message: response.data.choices[0].message.content,
                 type: "chatgpt",
